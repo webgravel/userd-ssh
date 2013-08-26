25e83a0ef05978da33f1cf9e8bca353557af66c3
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-26 16:21:59 +0200

    SSH server for userd

--- sshd.py
+++ sshd.py
@@ -14,6 +14,7 @@ from twisted.python import components
 from twisted.conch.ssh import session
 from twisted.cred import portal
 from twisted.internet import reactor
+from twisted.internet.error import ProcessExitedAlready
 from zope.interface import implements, providedBy
 
 import os
@@ -21,8 +22,7 @@ import pwd
 import hashlib
 import shlex
 
-class settings: pass
-settings = settings()
+import ssh_info
 
 def nothing():
     pass
@@ -39,8 +39,10 @@ class KeySession:
     def __init__(self, avatar):
         self.avatar = avatar
         self.pty_size = None
+        self.pty = None
 
     def execCommand(self, proto, exec_cmd):
+        print 'execCommand', proto, exec_cmd
         username, key_id = self.avatar.avatarId
         if key_id != '5f:62:fb:ec:4f:e1:82:e5:36:7d:76:2e:2c:50:8a:a5:e7:65:a5:6e:84:cb:11:b4:3b:a6:50:c1:b2:4c:a7:ae':
             raise ValueError('invalid key')
@@ -51,24 +53,52 @@ class KeySession:
                'attach', str(uid), '--']
         if self.pty_size:
             cmd += ['env', 'ROWS=%d' % self.pty_size[0], 'COLS=%d' % self.pty_size[1],
-                    '--', '/gravel/pkg/gravel-userd-ssh/pty-helper']
-        cmd += shlex.split(exec_cmd)
-        self.pty = reactor.spawnProcess(proto,
+                    '--', '/gravel/pkg/gravel-userd-ssh/pty-helper', 'bash', '--login']
+        else:
+            cmd += ['sh', '-c', exec_cmd]
+        print cmd
+        self.pty = reactor.spawnProcess(ProcessExitWorkaroundWrapper(proto),
                                         cmd[0], cmd, os.environ, '/')
+
         self.avatar.conn.transport.transport.setTcpNoDelay(1)
 
     def getPty(self, term, windowSize, modes):
         self.pty_size = windowSize
         self.pty_term = term
 
+    def windowChanged(self, windowSize):
+        print 'windowChanged', windowSize
+
     def eofReceived(self):
-        self.pty.signalProcess('HUP')
+        if self.pty:
+            self.pty.closeStdin()
 
     def openShell(self, proto):
         self.execCommand(proto, 'bash')
 
     def closed(self):
-        pass
+        try:
+            self.pty.signalProcess('HUP')
+        except (OSError, ProcessExitedAlready):
+            pass
+        self.pty.loseConnection()
+
+class ProcessExitWorkaroundWrapper(object):
+    '''
+    Process seems to call processExited long before processEnded.
+    However SSHSessionProcessProtocol closes channel on processEnded.
+    '''
+    def __init__(self, obj):
+        self._obj = obj
+
+    def __getattr__(self, name):
+        return getattr(self._obj, name)
+
+    def processExited(self, reason=None):
+        return self.processEnded(reason)
+
+    def childDataReceived(self, a, data):
+        return self._obj.childDataReceived(a, data)
 
 components.registerAdapter(KeySession, KeyConchUser, session.ISession)
 
@@ -94,10 +124,7 @@ class KeyChecker(object):
         id = get_ssh_key_fingerprint(key)
         return (credentials.username, id)
 
-def main(keys_path, username_get, gid, port=2022):
-    settings.username_get = username_get
-    settings.gid = gid
-
+def main(keys_path, port):
     with open(keys_path + '/id_rsa') as privateBlobFile:
         privateBlob = privateBlobFile.read()
         privateKey = Key.fromString(data=privateBlob)
@@ -116,4 +143,4 @@ def main(keys_path, username_get, gid, port=2022):
     reactor.run()
 
 if __name__ == '__main__':
-    main('../../tmp', lambda name: 1002, 100)
+    main('../../tmp', 2022)
---
+++ start.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.7
+import sys
+import os
+
+sys.path.append('/gravel/pkg/gravel-common')
+sys.path.append('/gravel/pkg/gravel-node')
+
+import sshd
+import ssh_info
+import gravelnode
+
+key_dir = os.path.join('/gravel/system/node/ssh_keys')
+
+if not os.path.exists(key_dir + '/id_rsa'):
+    pub = gravelnode.master_call('get', 'sshd_rsa.pub', check_output=True)
+    priv = gravelnode.master_call('get', 'sshd_rsa', check_output=True)
+
+    if not pub or not priv:
+        raise ValueError('invalid ssh keys: pub, priv = %r, %r' % (pub, priv))
+
+    if not os.path.exists(key_dir):
+        os.mkdir(key_dir)
+        os.chmod(key_dir, 0o700)
+
+    with open(key_dir + '/id_rsa', 'w') as f:
+        f.write(priv)
+
+    with open(key_dir + '/id_rsa.pub', 'w') as f:
+        f.write(pub)
+
+sshd.main(key_dir, port=22)
